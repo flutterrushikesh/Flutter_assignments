7588dd1b8e05211d7e3c89b2dc4825a46dd2657f
flutterrushikesh <flutterrushikesh@gmail.com>
2024-02-06 23:07:27 +0530

    Complete

--- Stateful Assignment/Assignment21/interdynamic_listview/lib/main.dart
+++ Stateful Assignment/Assignment21/interdynamic_listview/lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'digit.dart';
 
-void main()=> runApp(const MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  @override 
-  Widget build(BuildContext contex ) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home:DisplayDigit(),
+      home: DisplayDigit(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
