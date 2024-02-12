4278f1b82749a3aaa0be14f1c83b1281626ac3ec
flutterrushikesh <flutterrushikesh@gmail.com>
2024-02-12 12:17:07 +0530

    Functionality complete but modular class remaining

--- Stateful Assignment/quiz_app/lib/app.dart
+++ Stateful Assignment/quiz_app/lib/app.dart
@@ -1,76 +1,58 @@
 import 'package:flutter/material.dart';
-
-class Quiz extends StatefulWidget {
-  const Quiz({super.key});
-
+class QuizApp extends StatefulWidget {
+  const QuizApp({super.key});
   @override
-  State<Quiz> createState() => _QuizState();
+  State<QuizApp> createState() => _QuizAppState();
 }
 
-class _QuizState extends State<Quiz> {
+class Question {
+  final String? modelquestion;
+  final List<String>? options;
+  final int? answerIndex;
+
+  const Question(this.modelquestion, this.options, this.answerIndex);
+}
+
+class _QuizAppState extends State<QuizApp> {
   List allQuestions = [
     {
-      "question": "Who is the founder of Microsoft?",
-      "options": [
-        "Steve Jobs",
-        "Jeff Bezos",
-        "Bill Gates",
-        "Elon Musk",
-      ],
-      "answerIndex": 2,
-    },
-    {
-      "question": "Who is the founder of Apple?",
-      "option": [
-        "Steave Jobs",
-        "jeff Bezos",
-        "Bill Gates",
-        "Elon Musk",
-      ],
-      "answerIndex": 0,
-    },
-    {
-      "quetion": "Who is the founder of Amazon?",
-      "option": [
-        "Steave Jobs",
-        "Jeff Bezos",
-        "Bill Gates",
-        "Elon Musk",
-      ],
-      "answerIndex": 1,
-    },
-    {
-      "quetion": "Who is the founder of Tesla?",
-      "option": [
-        "Steave Jobs",
-        "Jeff Bezos",
-        "Bill Gates",
-        "Elon Musk",
-      ],
+      "question": "Which datatype hold the both value of int and double?",
+      "option": ["numeric", "int", "double", "num"],
       "answerIndex": 3,
     },
     {
-      "option": [
-        "Steave Jobs",
-        "Lary Page",
-        "Bill Gates",
-        "Elon Musk",
-      ],
-      "answerIndex": 1,
+      "question": "....... data type can be used to represent true or false",
+      "option": ["numeric", "int", "bool", "boolean"],
+      "answerIndex": 2,
     },
+    {
+      "question": "Which of the following is not arithematic operator?",
+      "option": ["#", "/", "+", "-"],
+      "answerIndex": 0,
+    },
+    {
+      "question": "....... property is used to check the runtime data",
+      "option": ["runtimeType", "typeOf", "instanceOf", "runtime"],
+      "answerIndex": 0,
+    },
+    {
+      "question": "Which data type used to declare string",
+      "option": ["char", "string", "String", "str"],
+      "answerIndex": 2,
+    }
   ];
 
   bool questionScreen = true;
   int questionIndex = 0;
-  int optionColor1 = -1;
-  int currentAnswerIndex = 0;
-  int selectedAnswerIndex = 0;
+  int selectedAnswerIndex = -1;
+  int optionColor = -1;
+  int correctAnswers = 0;
 
-  MaterialStatePropertyAll<Color?> buttonColor(int button) {
-    if (optionColor1 != -1) {
-      if (button == allQuestions[questionIndex]["answerIndex"]) {
+  MaterialStatePropertyAll<Color?> validateAnswer(int buttonIndex) {
+    if (selectedAnswerIndex != -1) {
+      if (buttonIndex == allQuestions[questionIndex]["answerIndex"]) {
         return const MaterialStatePropertyAll(Colors.green);
-      } else if (button == selectedAnswerIndex) {
+      } else if (buttonIndex == selectedAnswerIndex) {
         return const MaterialStatePropertyAll(Colors.red);
       } else {
         return const MaterialStatePropertyAll(null);
@@ -80,16 +62,37 @@ class _QuizState extends State<Quiz> {
     }
   }
 
-  Scaffold isQuestionScreen() {
+  void questionValidation() {
+    setState(() {
+      if (selectedAnswerIndex == -1) {
+        return;
+      }
+      if (selectedAnswerIndex == allQuestions[questionIndex]["answerIndex"]) {
+        correctAnswers++;
+      }
+      if (selectedAnswerIndex != -1) {
+        if (questionIndex < allQuestions.length - 1) {
+          setState(() {
+            questionIndex++;
+          });
+        } else {
+          questionIndex = 0;
+          selectedAnswerIndex = -1;
+          questionScreen = false;
+        }
+      }
+    });
+  }
+
+  Scaffold screen() {
     if (questionScreen == true) {
       return Scaffold(
         appBar: AppBar(
           title: const Text(
             "Quiz",
             style: TextStyle(
-              fontWeight: FontWeight.bold,
+              fontWeight: FontWeight.w700,
               fontSize: 30,
-              color: Colors.black,
             ),
           ),
           centerTitle: true,
@@ -97,120 +100,116 @@ class _QuizState extends State<Quiz> {
         ),
         body: Column(
           children: [
+            const SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const SizedBox(
-                  height: 10,
-                ),
                 const Text(
-                  "Question : ",
+                  "Qestions : ",
                   style: TextStyle(
-                    fontWeight: FontWeight.w500,
                     fontSize: 20,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
                 Text(
                   "${questionIndex + 1}/${allQuestions.length}",
                   style: const TextStyle(
-                    fontWeight: FontWeight.w500,
+                    fontWeight: FontWeight.w600,
                     fontSize: 20,
                   ),
                 ),
               ],
             ),
-            const SizedBox(
-              height: 30,
+            const SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "${allQuestions[questionIndex]["question"]}",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15,
+                  ),
+                ),
+              ],
             ),
+            const SizedBox(height: 20),
             SizedBox(
               height: 50,
-              width: 320,
-              child: Text(
-                allQuestions[questionIndex]["question"],
-                style: const TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 300,
-              height: 30,
+              width: 280,
               child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: validateAnswer(0),
+                ),
                 onPressed: () {
-                  if (optionColor1 == -1) {
-                    optionColor1 = 0;
+                  if (selectedAnswerIndex == -1) {
                     setState(() {
                       selectedAnswerIndex = 0;
                     });
                   }
                 },
-                style: ButtonStyle(
-                  backgroundColor: buttonColor(0),
+                child: Text(
+                  "A. ${allQuestions[questionIndex]["option"][0]}",
                 ),
-                child: Text("A. ${allQuestions[questionIndex]["options"][0]}"),
               ),
             ),
-            const SizedBox(height: 10),
+            const SizedBox(height: 15),
             SizedBox(
-              width: 300,
-              height: 30,
+              height: 50,
+              width: 280,
               child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: validateAnswer(1),
+                ),
                 onPressed: () {
-                  if (optionColor1 == -1) {
-                    optionColor1 = 0;
+                  if (selectedAnswerIndex == -1) {
                     setState(() {
                       selectedAnswerIndex = 1;
                     });
                   }
                 },
-                style: ButtonStyle(
-                  backgroundColor: buttonColor(1),
+                child: Text(
+                  "B. ${allQuestions[questionIndex]["option"][1]}",
                 ),
-                child: Text("B. ${allQuestions[questionIndex]["options"][1]}"),
               ),
             ),
-            const SizedBox(
-              height: 10,
-            ),
+            const SizedBox(height: 15),
             SizedBox(
-              height: 30,
-              width: 300,
+              height: 50,
+              width: 280,
               child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: validateAnswer(2),
+                ),
                 onPressed: () {
-                  if (optionColor1 == -1) {
-                    optionColor1 = 0;
+                  if (selectedAnswerIndex == -1) {
                     setState(() {
                       selectedAnswerIndex = 2;
                     });
                   }
                 },
-                style: ButtonStyle(
-                  backgroundColor: buttonColor(2),
+                child: Text(
+                  "C. ${allQuestions[questionIndex]["option"][2]}",
                 ),
-                child: Text("C. ${allQuestions[questionIndex]["options"][2]}"),
               ),
             ),
-            const SizedBox(
-              height: 15,
-            ),
+            const SizedBox(height: 15),
             SizedBox(
-              height: 30,
-              width: 300,
+              height: 50,
+              width: 280,
               child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: validateAnswer(3),
+                ),
                 onPressed: () {
-                  if (optionColor1 == -1) {
-                    optionColor1 = 0;
+                  if (selectedAnswerIndex == -1) {
                     setState(() {
                       selectedAnswerIndex = 3;
                     });
                   }
                 },
-                style: ButtonStyle(
-                  backgroundColor: buttonColor(3),
-                ),
                 child: Text(
-                  "D. ${allQuestions[questionIndex]["options"][3]}",
+                  "D. ${allQuestions[questionIndex]["option"][3]}",
                 ),
               ),
             ),
@@ -218,21 +217,63 @@ class _QuizState extends State<Quiz> {
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            
+            optionColor = -1;
+
+            setState(() {
+              questionValidation();
+              selectedAnswerIndex = -1;
+            });
           },
-          child: const Icon(
-            Icons.arrow_forward_outlined,
-            size: 25,
-          ),
+          child: const Icon(Icons.arrow_forward_outlined),
         ),
       );
     } else {
-      return const Scaffold();
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Result",
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 27,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Image.network(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmoGE8pGXE4aUURpYIETqvw6W5RZB-iVvKdw&usqp=CAU',
+                height: 300,
+                width: 200,
+              ),
+              const Text(
+                "Congragulations!!!",
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 30,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "You have completed $correctAnswers/${allQuestions.length}",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return isQuestionScreen();
+    return screen();
   }
 }
---
+++ Stateful Assignment/quiz_app/lib/modularclass.dart
@@ -0,0 +1,26 @@
+/*const Question(
+      question: "Which datatype hold the both value of int and double?",
+      options: ["numeric", "int", "double", "num"],
+      answerIndex: 3,
+    ),
+    const Question(
+      question: "....... data type can be used to represent true or false",
+      options: ["numeric", "int", "bool", "boolean"],
+      answerIndex: 2,
+    ),
+    const Question(
+      question: "Which of the following is not arithematic operator?",
+      options: ["#", "/", "+", "-"],
+      answerIndex: 0,
+    ),
+    const Question(
+      question: "....... property is used to check the runtime data",
+      options: ["runtimeType", "typeOf", "instanceOf", "runtime"],
+      answerIndex: 0,
+    ),
+    const Question(
+      question: "Which data type used to declare string",
+      options: ["char", "string", "String", "str"],
+      answerIndex: 2,
+    ),
+  ];*/
