c91131c91522c982ae928171fe53ae07ddf07c06
flutterrushikesh <flutterrushikesh@gmail.com>
2024-02-20 12:09:18 +0530

    dependancies

---
+++ LinkedIn posts/dreamcompany/README.md
@@ -0,0 +1,3 @@
+# dreamcompany
+
+A new Flutter project.
---
+++ LinkedIn posts/dreamcompany/android/app/src/main/kotlin/com/example/dreamcompany/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dreamcompany
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
