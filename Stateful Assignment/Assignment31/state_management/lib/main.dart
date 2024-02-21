b39e4b60b455a30817f753513795f39923feaaaa
flutterrushikesh <flutterrushikesh@gmail.com>
2024-02-21 12:44:40 +0530

    only init state declared

--- Stateful Assignment/Assignment31/state_management/lib/main.dart
+++ Stateful Assignment/Assignment31/state_management/lib/main.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: Management(),
+    );
+  }
+}
+
+class Management extends StatefulWidget {
+  const Management({super.key});
+  @override
+  State createState() {
+    print("In Create State");
+    return _ManagementState();
+  }
+}
+
+class _ManagementState extends State {
+
+  @override
+  void initState() {
+    print("In init State");
+    super.initState();
+  }
+
+  @override
+  void 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "State Management",
+          style: TextStyle(
+            fontSize: 23,
+          ),
         ),
       ),
     );
