9b8026af349a75c5b606d942597119131256481d
flutterrushikesh <flutterrushikesh@gmail.com>
2024-02-23 15:24:06 +0530

    complete

--- Stateful Assignment/Assignment33/dynamic_card_generate/lib/main.dart
+++ Stateful Assignment/Assignment33/dynamic_card_generate/lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
+import 'dynamiccard.dart';
 
-void main() {
-  runApp(const MainApp());
-}
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+void main() => runApp(const MyApp());
 
-  @override
-  Widget build(BuildContext context) {
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override 
+  Widget build(BuildContext context){
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home:DynamicCart(),
+      debugShowCheckedModeBanner:false,
     );
   }
 }
