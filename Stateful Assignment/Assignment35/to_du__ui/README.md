636fd173f5a76ca4d3a7e3084e7f076cfbb57c79
flutterrushikesh <flutterrushikesh@gmail.com>
2024-02-25 09:47:34 +0530

    dependancies

---
+++ Stateful Assignment/Assignment35/to_du__ui/README.md
@@ -0,0 +1,3 @@
+# to_du__ui
+
+A new Flutter project.
---
+++ Stateful Assignment/Assignment35/to_du__ui/android/app/src/main/kotlin/com/example/to_du__ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.to_du__ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
