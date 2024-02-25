7bbaba15d4f122382e52ae93a7158db30b74140d
flutterrushikesh <flutterrushikesh@gmail.com>
2024-02-25 12:08:52 +0530

    dependancies

--- Stateful Assignment/Assignment36/booton_sheet_demo/lib/main.dart
+++ Stateful Assignment/Assignment36/booton_sheet_demo/lib/main.dart
@@ -9,11 +9,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text("Botton sheet"),
         ),
+        floatingActionButton: FloatingActionButton(onPressed: (){
+          showModalBottomSheet(context: context, builder: BuildContext context){
+            return Column(),
+          }
+        },),
       ),
     );
   }
