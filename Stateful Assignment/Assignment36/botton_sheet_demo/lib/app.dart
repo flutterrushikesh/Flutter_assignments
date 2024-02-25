fa69d92a0a05db697ad8771290e92f520104cc26
flutterrushikesh <flutterrushikesh@gmail.com>
2024-02-26 02:00:22 +0530

    Complete

--- Stateful Assignment/Assignment36/botton_sheet_demo/lib/app.dart
+++ Stateful Assignment/Assignment36/botton_sheet_demo/lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:google_fonts/google_fonts.dart';
 
 class BottomSheetDemo extends StatefulWidget {
@@ -8,10 +9,25 @@ class BottomSheetDemo extends StatefulWidget {
 }
 
 class _BottomSheetDemoState extends State {
-  final TextEditingController _taskEditingController = TextEditingController();
+  final TextEditingController _titleEditingController = TextEditingController();
+  final TextEditingController _descriptionEditingController =
+      TextEditingController();
 
-  final FocusNode _taskFocusNode = FocusNode();
+  final TextEditingController _dateEditingController = TextEditingController();
 
+  final FocusNode _titleFocusNode = FocusNode();
+  final FocusNode _descriptionFocusNode = FocusNode();
+  final FocusNode _dateFocusNode = FocusNode();
+
+  List userData = [];
+
+  List listOfColor = const [
+    Color.fromRGBO(250, 232, 232, 1),
+    Color.fromRGBO(232, 237, 250, 1),
+    Color.fromRGBO(250, 249, 232, 1),
+    Color.fromRGBO(250, 232, 250, 1),
+    Color.fromRGBO(250, 232, 232, 1),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +51,7 @@ class _BottomSheetDemoState extends State {
                 child: Column(
                   children: [
                     const SizedBox(
-                      height: 25,
+                      height: 15,
                     ),
                     Text(
                       "Create Task",
@@ -44,30 +60,204 @@ class _BottomSheetDemoState extends State {
                         fontSize: 22,
                       ),
                     ),
-                    const SizedBox(height: 30),
-                    SizedBox(
+                    const SizedBox(height: 8),
+                    Row(
+                      // mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const SizedBox(width: 15),
+                        Text(
+                          "Title",
+                          style: GoogleFonts.quicksand(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17,
+                            color: const Color.fromRGBO(0, 139, 149, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(
+                        top: 5,
+                        left: 15,
+                        right: 15,
+                        bottom: 15,
+                      ),
                       child: TextField(
-                        controller: _taskEditingController,
-                        focusNode: _taskFocusNode,
+                        controller: _titleEditingController,
+                        focusNode: _titleFocusNode,
                         autofocus: false,
-                        decoration: InputDecoration(
-                          hintText: "Task",
-                          border: InputBorder.none,
+                        enabled: true,
+                        decoration: const InputDecoration(
                           enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(
-                              20,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(
+                                10,
+                              ),
                             ),
-                            borderSide: const BorderSide(
-                              color: Colors.blue,
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(0, 139, 149, 1),
                             ),
                           ),
                           focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(
-                              20,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(
+                                10,
+                              ),
                             ),
                             borderSide: BorderSide(
-                              color: Colors.red,
+                              color: Color.fromRGBO(0, 137, 149, 1),
                             ),
+                          ),
+                        ),
+                        cursorColor: Colors.black,
+                        textInputAction: TextInputAction.done,
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: (value) => value,
+                        onSubmitted: (value) => value,
+                      ),
+                    ),
+                    Row(
+                      // mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const SizedBox(width: 15),
+                        Text(
+                          "Description",
+                          style: GoogleFonts.quicksand(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17,
+                            color: const Color.fromRGBO(0, 139, 149, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(
+                        top: 5,
+                        left: 15,
+                        right: 15,
+                        bottom: 15,
+                      ),
+                      child: TextField(
+                        controller: _descriptionEditingController,
+                        focusNode: _descriptionFocusNode,
+                        autofocus: false,
+                        enabled: true,
+                        decoration: const InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(
+                                10,
+                              ),
+                            ),
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(0, 139, 149, 1),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(
+                                10,
+                              ),
+                            ),
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(0, 137, 149, 1),
+                            ),
+                          ),
+                        ),
+                        cursorColor: Colors.black,
+                        textInputAction: TextInputAction.done,
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: (value) => value,
+                        onSubmitted: (value) => value,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        const SizedBox(
+                          width: 15,
+                        ),
+                        Text(
+                          "Date",
+                          style: GoogleFonts.quicksand(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17,
+                            color: const Color.fromRGBO(
+                              0,
+                              139,
+                              149,
+                              1,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(
+                        top: 5,
+                        bottom: 15,
+                        left: 15,
+                        right: 15,
+                      ),
+                      child: TextField(
+                        controller: _dateEditingController,
+                        focusNode: _dateFocusNode,
+                        autofocus: false,
+                        enabled: true,
+                        decoration: const InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(
+                                1,
+                                139,
+                                147,
+                                1,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                      width: 300,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            userData.add({
+                              "title": _titleEditingController.text,
+                              "description": _descriptionEditingController.text,
+                              "date": _dateEditingController.text,
+                            });
+                          });
+                        },
+                        style: const ButtonStyle(
+                          shape: MaterialStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(
+                                  10,
+                                ),
+                              ),
+                            ),
+                          ),
+                          backgroundColor: MaterialStatePropertyAll(
+                            Color.fromRGBO(
+                              0,
+                              137,
+                              147,
+                              1,
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          "Submit",
+                          style: GoogleFonts.inter(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 22,
+                            color: Colors.white,
                           ),
                         ),
                       ),
@@ -79,6 +269,81 @@ class _BottomSheetDemoState extends State {
           );
         },
         child: const Icon(Icons.add),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView.builder(
+            itemCount: userData.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                children: [
+                  Container(
+                    height: 130,
+                    width: 360,
+                    margin: const EdgeInsets.all(15),
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: listOfColor[index % listOfColor.length],
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(
+                          10,
+                        ),
+                      ),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 9,
+                          spreadRadius: 1,
+                          offset: Offset(-7, 8),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Title : ${userData[index]["title"]}",
+                          style: GoogleFonts.quicksand(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Text(
+                          "Description: ${userData[index]["description"]}",
+                          style: GoogleFonts.quicksand(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Row(
+                          children: [
+                            Text(
+                              "Date : ${userData[index]["date"]}",
+                              style: GoogleFonts.quicksand(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 14,
+                              ),
+                            ),
+                            const Spacer(),
+                            const Icon(
+                              Icons.edit,
+                              size: 30,
+                            ),
+                            const SizedBox(width: 10),
+                            const Icon(
+                              Icons.delete_outlined,
+                              size: 30,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            }),
       ),
     );
   }
