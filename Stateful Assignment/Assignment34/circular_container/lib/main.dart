eb257e7f6568f7c61813bebe57565f93df4268dc
flutterrushikesh <flutterrushikesh@gmail.com>
2024-02-25 09:37:37 +0530

    complete

--- Stateful Assignment/Assignment34/circular_container/lib/main.dart
+++ Stateful Assignment/Assignment34/circular_container/lib/main.dart
@@ -9,10 +9,31 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Container(
+            height: 150,
+            width: 150,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(190, 188, 182, 1),
+              shape: BoxShape.circle,
+              
+              border: Border.all(
+                width: 2,
+                color: const Color.fromRGBO(0, 0, 0, 1),
+              ),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.red,
+                  offset: Offset(20, -20),
+                  blurRadius: 20,
+                )
+              ],
+              
+            ),
+          ),
         ),
       ),
     );
