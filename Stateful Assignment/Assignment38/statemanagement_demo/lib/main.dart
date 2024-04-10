882d3c195f6022274265971001bbf13ce61c43b1
flutterrushikesh <flutterrushikesh@gmail.com>
2024-04-10 09:38:03 +0530

    Complete

--- Stateful Assignment/Assignment38/statemanagement_demo/lib/main.dart
+++ Stateful Assignment/Assignment38/statemanagement_demo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +11,62 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: Course(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Course extends StatefulWidget {
+  const Course({super.key});
+  @override
+  State createState() => _CourseState();
+}
+
+class _CourseState extends State {
+  int javaCounter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Course counter"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            GestureDetector(
+              onTap: () {
+                javaCounter++;
+                setState(() {});
+              },
+              child: Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  border: Border.all(),
+                ),
+                child: const Center(
+                  child: Text(
+                    "Java",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              "$javaCounter",
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
         ),
       ),
     );
