20b5ea577acf2f0dc1f9ddc208977d375fca1ec3
flutterrushikesh <flutterrushikesh@gmail.com>
2024-04-13 00:19:48 +0530

    Complete

--- Stateful Assignment/Assignment40/gridviewdemo/lib/main.dart
+++ Stateful Assignment/Assignment40/gridviewdemo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,52 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text("GridView"),
+          backgroundColor: Colors.blue,
+        ),
+        body: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          padding: const EdgeInsets.all(10),
+          children: [
+            Container(
+              color: Colors.amber,
+            ),
+            Container(
+              color: Colors.lime,
+            ),
+            Container(
+              color: Colors.black,
+            ),
+            Container(
+              color: Colors.pink,
+            ),
+            Container(
+              color: Colors.green,
+            ),
+            Container(
+              color: Colors.purple,
+            ),
+            Container(
+              color: Colors.grey,
+            ),
+            Container(
+              color: Color.fromARGB(255, 21, 222, 244),
+            ),
+            Container(
+              color: Color.fromARGB(255, 30, 127, 11),
+            ),
+            Container(
+              color: Color.fromARGB(255, 238, 163, 12),
+            ),
+          ],
         ),
       ),
     );
