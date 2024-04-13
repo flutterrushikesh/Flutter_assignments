499b00947359337de17fcb8ebcc5b4cfacb35d23
flutterrushikesh <flutterrushikesh@gmail.com>
2024-04-14 01:50:52 +0530

    Complete

--- Stateful Assignment/Assignment42/alert_dialog_demo/lib/main.dart
+++ Stateful Assignment/Assignment42/alert_dialog_demo/lib/main.dart
@@ -10,11 +10,88 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: MyAlertDialog(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyAlertDialog extends StatefulWidget {
+  const MyAlertDialog({super.key});
+  @override
+  State createState() => _MyAlertDialogState();
+}
+
+class _MyAlertDialogState extends State {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Alert box"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Table of 2",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "$counter",
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                  counter = 0;
+                },
+                child: const Text(
+                  "Reset",
+                  style: TextStyle(fontSize: 15),
+                ))
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          alertBox();
+        },
+        child: const Icon(Icons.add, size: 35),
+      ),
     );
+  }
+
+  void alertBox() {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Are you sure to add  "),
+            content: const Text("add value by 2"),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    counter += 2;
+                  });
+                  Navigator.of(context).pop();
+                },
+                child: const Text("Add"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("Not now"),
+              )
+            ],
+          );
+        });
   }
 }
