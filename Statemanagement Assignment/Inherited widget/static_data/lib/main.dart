6a0383a7e205b5c01a948ff2f25f700cce605f90
flutterrushikesh <flutterrushikesh@gmail.com>
2024-04-18 12:38:52 +0530

    Complete

--- Statemanagement Assignment/Inherited widget/static_data/lib/main.dart
+++ Statemanagement Assignment/Inherited widget/static_data/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:static_data/staticdata.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,44 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return const PassData(
+      name: "Virat Kohali",
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(
+            child: MyText(),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class MyText extends StatelessWidget {
+  const MyText({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      // mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(height: 350),
+        Container(
+          margin: const EdgeInsets.all(100),
+          child: Text(
+            PassData.of(context).name,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        const Spacer(),
+        const Text(
+          "[Note this data retrives from inherited \nwidget not hard coded]",
+          style: TextStyle(fontSize: 10),
+        )
+      ],
     );
   }
 }
